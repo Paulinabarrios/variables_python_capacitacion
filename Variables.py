3aa1c52c8c52d6a6ec61b89daae36b69e6bd6eea
Paulina Barrios <Paulina.barrios.godoy@gmail.com>
2023-04-26 17:06:46 -0400

    first commit

---
+++ Variables.py
@@ -0,0 +1,11 @@
+# Declarando variables
+nombre ="Paulina"
+edad = 25
+valor_dolar = 810.20
+estudias = True
+
+# imprimiendo nuestras variables
+print(nombre)
+print(edad)
+print(valor_dolar)
+print(estudias)
---
+++ operadores.py
@@ -0,0 +1,26 @@
+# operadores matematico
+# realizar la suma
+# datos de entrada
+variable1 =int(input("ingrese el primer numero: "))
+variable2 = int(input("ingrese el segunod numero: "))
+# procesando los datos
+suma = variable1 + variable2
+resta = variable1 - variable2
+multiplicacion = variable1 * variable2
+division = variable1 / variable2 
+modulo = variable1%variable2
+potencia = variable1**variable2
+# salida del proceso
+print(suma)
+print (resta)
+print(multiplicacion)
+print(division)
+print(modulo)
+print (potencia)
+# salida con interpoblacion
+print(f"la suma es: {suma}")
+print(f"la resta es: {resta}")
+print(f"la multiplicacion es: {multiplicacion}")
+print(f"la division es: {division}")
+print(f"la modulo es: {modulo}")
+print(f"la potencia es: {potencia}")
